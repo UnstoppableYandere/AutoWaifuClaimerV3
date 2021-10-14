4fa71209299092ce7d8bfe0267cf639bd07e91c1
UnstoppableYandere <92499950+UnstoppableYandere@users.noreply.github.com>
2021-10-14 15:00:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Discord IDs
 # Replace values with your own.
 MUDAE_ID = 898293438674509844  # ID of Mudae bot
-CHANNEL_ID = 898292293507883058  # ID of claiming channel
+CHANNEL_ID = 838923953660755988  # ID of claiming channel
 SERVER_ID = 891162703840837632  # ID of Discord server
 USER_ID = 442168558898642946  # ID of main user
 
